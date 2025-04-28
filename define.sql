45830ad77101cc6942cb0beca1a4093f823b45a0
Jérémie Dupuis <JeremieD@users.noreply.github.com>
2025-04-27 22:07:51 -0400

    Add Commandes

--- define.sql
+++ define.sql
@@ -2,30 +2,39 @@
  * Script de création des tables.
  */
 
-DROP TABLE IF EXISTS Emprunts, Livres, Adherents;
+DROP TABLE IF EXISTS Emprunts, Livres, Adherents, Commandes;
 
 CREATE TABLE Adherents (
   adherent_id serial PRIMARY KEY,
-  nom varchar(255),
-  adresse varchar(255)
+  nom VARCHAR(255),
+  adresse VARCHAR(255)
 );
 
 CREATE TABLE Livres (
-  isbn numeric(13, 0) PRIMARY KEY,
-  titre varchar(255),
-  auteur varchar(255),
-  editeur varchar(255),
-  annee smallint,
-  genre varchar(255),
-  langue char(2)
+  isbn NUMERIC(13, 0) PRIMARY KEY,
+  titre VARCHAR(255),
+  auteur VARCHAR(255),
+  editeur VARCHAR(255),
+  annee SMALLINT,
+  genre VARCHAR(255),
+  langue CHAR(2)
 );
 
 CREATE TABLE Emprunts (
   emprunt_id serial PRIMARY KEY,
   adherent_id serial,
-  isbn numeric(13, 0) NOT NULL,
-  date_emprunt date NOT NULL,
-  date_retour date,
+  isbn NUMERIC(13, 0) NOT NULL,
+  date_emprunt DATE NOT NULL,
+  date_retour DATE,
   FOREIGN KEY (adherent_id) REFERENCES Adherents(adherent_id),
   FOREIGN KEY (isbn) REFERENCES Livres(isbn)
+);
+
+CREATE TABLE Commandes (
+  id serial PRIMARY KEY,
+  adherent_id serial,
+  isbn NUMERIC (13, 0) NOT NULL,
+  statut VARCHAR(255) DEFAULT 'Initiée',
+  date_creation DATE DEFAULT CURRENT_DATE NOT NULL,
+  date_maj DATE
 );
